5925010b6df3a31990fe107e30c055bd362d148d
Mark Landry <markolandry@gmail.com>
2014-02-04 15:23:12 -0400

    Fixed cookies

--- javascript/survey.js
+++ javascript/survey.js
@@ -1,4 +1,6 @@
 $(document).ready( function() {
 	console.log("page ready");
-	$.cookie("my_cookie",1);
+	$.cookie("my_cookie",1,{ path : '/'},{ expires : 10 } );
+	console.log($.cookie("my_cookie"));
+	alert($.cookie("my_cookie"))
 } )
