394651e3540a48c89f5d15102ae71a947d8536bb
ZhishenYang <yangzhishen188@gmail.com>
2014-02-12 17:15:27 -0400

    survey page add ID, recommendation.js changed

--- javascript/recommendation.js
+++ javascript/recommendation.js
@@ -3,10 +3,29 @@ var  tags = $.cookie("tags");
 	if(tags == null){
 		tags ="";
 	}
-
+	
 	if(tags.indexOf("lowCardio") == -1){
 		console.log("remiving ")
-		$("lowCardio").remove();
+		$("mennu").remove("lowCardio");
 	}
-
+	if(tags.indexOf("lowStrength") == -1){
+		console.log("remiving ")
+		$("lowStrength").remove();
+	}
+	
+	if(tags.indexOf("highCardio") == -1){
+		console.log("remiving ")
+		$("highCardio").remove();
+	}
+	if(tags.indexOf("highStrength") == -1){
+		console.log("remiving ")
+		$("highStrength").remove();
+	}
+	
+	if(tags.indexOf("highIntensity") == -1){
+		console.log("remiving ")
+		$("highIntensity").remove();
+	}
+	
+	
 })
