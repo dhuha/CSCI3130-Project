65a9870edfb6ab9347151a0a789cec1dc372c0dc
sheroz-siraj <sherozmail@gmail.com>
2014-03-06 11:12:38 -0400

    added outlines for testing the reccomendation

---
+++ test/reccomendationSpec.js
@@ -0,0 +1,16 @@
+describe("Reccomendations", function() {
+    beforeEach(function() {
+    });
+    it('should fail',function(){
+        expect(true).toEqual(false);
+    })
+    it('should get non empty tags',function(){
+
+    });
+    it('should show reccomendations',function(){
+
+    });
+    it('should show graph'function(){
+
+    });
+})          
