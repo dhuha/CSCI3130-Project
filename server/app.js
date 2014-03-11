10cbf346bfb64c753d35999456f8cf84b27a4935
ZhishenYang <yangzhishen188@gmail.com>
2014-03-11 15:26:03 -0300

    Fixed mongo database schemas

--- server/app.js
+++ server/app.js
@@ -5,20 +5,20 @@
  * CSCI3130 Winter Semester 2014
  *
  */
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var db = mongoose.connection;
-var Users;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback () {
+ var express = require('express');
+ var app = express();
+ var mongoose = require('mongoose');
+ var Schema = mongoose.Schema;
+ var db = mongoose.connection;
+ var Users;
+ db.on('error', console.error.bind(console, 'connection error:'));
+ db.once('open', function callback () {
   var userSchema = new Schema({
-  username: String,
-  tags: String
-});
-Users = mongoose.model('User', userSchema);
+    username: String,
+    tags: String
   });
+  Users = mongoose.model('User', userSchema);
+});
 
 
 
@@ -42,6 +42,7 @@ var enableCORS = function(req, res, next) {
 
 // enable CORS!
 app.use(enableCORS);
+app.user(bodyParser());
 
 
 
@@ -50,14 +51,14 @@ app.get('/hello',function(req,res){
 });
 
 app.get('/user/tags',function(req,res){
-    console.log('tag request');
-    return  userModel.findOne({ 'username': 'TEST'},'username tags', function(err, u){
-      if(!err){
-     	 	return res.send(u.tags);	
-      }else{
-      	return res.send(err);	
-      }
-    });
+  console.log('tag request');
+  return  userModel.findOne({ 'username': 'TEST'},'username tags', function(err, u){
+    if(!err){
+      return res.send(u.tags);	
+    }else{
+     return res.send(err);	
+   }
+ });
 });
 
 app.post('/user/add',function(req,res){
@@ -67,12 +68,12 @@ app.post('/user/add',function(req,res){
 	});
   nUser.save(function (err) {
   	if (!err){
-    	return console.log('new user');
-    }else{
-      return console.log(err);
-    }
-  });
- 	return res.send(nUser);
+     return console.log('new user');
+   }else{
+    return console.log(err);
+  }
+});
+  return res.send(nUser);
 });
 
 var server = app.listen(30000,function(){
