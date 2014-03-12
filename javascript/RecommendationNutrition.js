e1743204382545212a60d2bbf9baec6f33d0727a
sheroz-siraj <sherozmail@gmail.com>
2014-03-12 17:25:34 -0300

    Added another function lowStrength()
    Recommendations to improve Strength (Nutrition)

--- javascript/RecommendationNutrition.js
+++ javascript/RecommendationNutrition.js
@@ -1,46 +1,94 @@
-// JavaScript Document
 
-function lowcardio(){
-var recommendations = ["Whole Eggs. Cheap & rich source of protein: 7g/egg. The yolk contains most nutrients: half the protein, vitamins A/D/E and cholesterol to naturally increase your testosterone levels"
+function lowStrength(){
+	var recommendations = ["Whole Eggs. Cheap & rich source of protein: 7g/egg. The yolk contains most nutrients: half the protein, vitamins A/D/E and cholesterol to naturally increase your testosterone levels"
 
-,"Fish Oil. Reduces inflammation (joints/skin), lowers body fat and increases testosterone levels. You need 9000mg EPA/DHA per day. Since youâ€™ll probably struggle to get that from eating fatty fish, consider a fish oil supplement."
+	,"Fish Oil. Reduces inflammation (joints/skin), lowers body fat and increases testosterone levels. You need 9000mg EPA/DHA per day. Since youâ€™ll probably struggle to get that from eating fatty fish, consider a fish oil supplement."
 
-,"Wild Salmon. One of the best sources of omega-3 fatty acids that also gets you 20g protein per 100g serving. Farm raised salmon is, however, omega-3 deficient: itâ€™s corn/grain fed. Go with wild salmon."
+	,"Wild Salmon. One of the best sources of omega-3 fatty acids that also gets you 20g protein per 100g serving. Farm raised salmon is, however, omega-3 deficient: itâ€™s corn/grain fed. Go with wild salmon."
 
-,"Berries. Strong antioxidants that prevent cancer, heart & eye diseases. Any kind works: cranberries, raspberries, blackberries, blueberries, etc. Buy fresh or frozen berries and mix with oatmeal."
+	,"Berries. Strong antioxidants that prevent cancer, heart & eye diseases. Any kind works: cranberries, raspberries, blackberries, blueberries, etc. Buy fresh or frozen berries and mix with oatmeal."
 
-,"Yogurt. Contain bacteria that improve your gastrointestinal health. Donâ€™t buy frozen yogurt or yogurt with added sugar and fruits at the bottom. Get plain low fat yogurt. Eat it with berries & flax seeds."
+	,"Yogurt. Contain bacteria that improve your gastrointestinal health. Donâ€™t buy frozen yogurt or yogurt with added sugar and fruits at the bottom. Get plain low fat yogurt. Eat it with berries & flax seeds."
 
-,"Flax Seeds. Source of fiber, protein & omega-3. Grind the flax seeds to get the most out of them. Take 1 tbsp with yogurt & berries before going to bed. Stay away from flax oil: itâ€™s unstable and contains no fiber."
+	,"Flax Seeds. Source of fiber, protein & omega-3. Grind the flax seeds to get the most out of them. Take 1 tbsp with yogurt & berries before going to bed. Stay away from flax oil: itâ€™s unstable and contains no fiber."
 
-,"Extra Virgin Olive Oil. 70% monounsaturated fats that protect against heart diseases and cancer. Add 1-2 tbsp olive oil to your salads. Buy Extra Virgin Olive Oil: it contains more polyphenols and tastes better."
+	,"Extra Virgin Olive Oil. 70% monounsaturated fats that protect against heart diseases and cancer. Add 1-2 tbsp olive oil to your salads. Buy Extra Virgin Olive Oil: it contains more polyphenols and tastes better."
 
-,"Mixed Nuts. Contain mono- & polyunsaturated fats, proteins, fiber, vitamin E, zinc, potassium, magnesium, etc. Mixed nuts are caloric dense, great if youâ€™re askinny guy who wants to gain weight."
+	,"Mixed Nuts. Contain mono- & polyunsaturated fats, proteins, fiber, vitamin E, zinc, potassium, magnesium, etc. Mixed nuts are caloric dense, great if youâ€™re askinny guy who wants to gain weight."
 
-,"Red Meat. Protein, vitamin B12, heme iron, zinc, creatine, carnosine and even omega-3 if you eat grass-fed beef. Eat steaks & hamburgers from top round or sirloin. Read Dr. Lonnie Loweryâ€™s article on Meat."
+	,"Red Meat. Protein, vitamin B12, heme iron, zinc, creatine, carnosine and even omega-3 if you eat grass-fed beef. Eat steaks & hamburgers from top round or sirloin. Read Dr. Lonnie Loweryâ€™s article on Meat."
 
-,"Broccoli. High in cancer-fighting phytochemicals and anti-estrogenic indoles. Broccoli is also high in soluble fiber and low calorie, helping fat loss. Eat othercruciferous vegetables for a change: cabbage, bok choy, cauliflower, kale."
+	,"Broccoli. High in cancer-fighting phytochemicals and anti-estrogenic indoles. Broccoli is also high in soluble fiber and low calorie, helping fat loss. Eat othercruciferous vegetables for a change: cabbage, bok choy, cauliflower, kale."
 
-,"Spinach. One of the most alkaline foods. Spinach prevents muscle & bone loss, but also cancer and heart diseases because of its high nutrient profile. Try one of the spinach recipes I shared a while back."
+	,"Spinach. One of the most alkaline foods. Spinach prevents muscle & bone loss, but also cancer and heart diseases because of its high nutrient profile. Try one of the spinach recipes I shared a while back."
 
-,"Turkey. If you donâ€™t believe saturated fat is good for you, try white turkey. The leanest beef has about 4.5g saturated fat/100g, while white turkey has close to 0g (that why itâ€™s so dry). Eat turkey with spinach & quinoa."
+	,"Turkey. If you donâ€™t believe saturated fat is good for you, try white turkey. The leanest beef has about 4.5g saturated fat/100g, while white turkey has close to 0g (that why itâ€™s so dry). Eat turkey with spinach & quinoa."
 
-,"Quinoa. South American â€œking of grainsâ€. Quinoa is higher in fiber & protein than rice or oats, tastes a lot better and is gluten free. Buy the whiter grain, itâ€™s better quality. Eat it post workout with meat & spinach."
+	,"Quinoa. South American â€œking of grainsâ€. Quinoa is higher in fiber & protein than rice or oats, tastes a lot better and is gluten free. Buy the whiter grain, itâ€™s better quality. Eat it post workout with meat & spinach."
 
-,"Oats. Reduce cholesterol, provide you with low-gi carbs for energy, and high in soluble fiber. Try this post workout shake of whey & oats."
+	,"Oats. Reduce cholesterol, provide you with low-gi carbs for energy, and high in soluble fiber. Try this post workout shake of whey & oats."
 
-,"Tomatoes. High in lycopene, which prevents cancer. The lycopene in tomato paste is 4 times more bioavailable than in fresh tomatoes. Have pizza or pasta with tomato sauce & olive oil post strength training."
+	,"Tomatoes. High in lycopene, which prevents cancer. The lycopene in tomato paste is 4 times more bioavailable than in fresh tomatoes. Have pizza or pasta with tomato sauce & olive oil post strength training."
 
-,"Oranges. Vitamin C to fight diseases, magnesium to lower blood pressure, anti-oxidant beta-carotenes, etc. Quit drinking processed orange juice which often has added sugars. Eat oranges or make your own orange juice."
+	,"Oranges. Vitamin C to fight diseases, magnesium to lower blood pressure, anti-oxidant beta-carotenes, etc. Quit drinking processed orange juice which often has added sugars. Eat oranges or make your own orange juice."
 
-,"Apples. Pectin in apples helps weight loss by increasing satiety. Apples are also the strongest antioxidiant after cranberries (eat the peels). Unfortunately apples are one of the most pesticide-contaminated fruits. Go organic."
+	,"Apples. Pectin in apples helps weight loss by increasing satiety. Apples are also the strongest antioxidiant after cranberries (eat the peels). Unfortunately apples are one of the most pesticide-contaminated fruits. Go organic."
 
-,"Carrots. Their huge vitamin A content improves eye-health, especially night vision. Carrots are also rich in fiber, low calorie and taste good, even raw."
+	,"Carrots. Their huge vitamin A content improves eye-health, especially night vision. Carrots are also rich in fiber, low calorie and taste good, even raw."
 
-,"Water. Your body holds water if you donâ€™t drink enough. Drinking prevents water retention, helps muscle recovery and prevents dehydration from strength training. Get a brita filter and drink 2 cups of water with each meal."
+	,"Water. Your body holds water if you donâ€™t drink enough. Drinking prevents water retention, helps muscle recovery and prevents dehydration from strength training. Get a brita filter and drink 2 cups of water with each meal."
 
-,"Green Tea. Strong antioxidant and natural diuretic. Green tea also speeds up fat loss, prevents cancer and improves blood sugar & circulation. Drink green tea in the morning instead of coffee. Real green tea, not the teabags."];
-var arrayLen = recommendations.length; 
-var randomNumber = Math.floor((Math.random()* arrayLen)+0);
-alert(recommendations[randomNumber]);	
+	,"Green Tea. Strong antioxidant and natural diuretic. Green tea also speeds up fat loss, prevents cancer and improves blood sugar & circulation. Drink green tea in the morning instead of coffee. Real green tea, not the teabags."];
+	var arrayLen = recommendations.length; 
+	var randomNumber = Math.floor((Math.random()* arrayLen)+0);
+	return recommendations[randomNumber];	
+}
+
+
+
+function lowCardio(){
+var recommendations = ["Salmon. Omega-3 fatty acids. Grill salmon with a yummy rub or marinade. Save a chunk to chop for a pasta or salad later on.",
+
+"Flaxseed (ground). Omega-3 fatty acids; fiber, phytoestrogens.Ground flaxseed hides easily in all sorts of foods -- yogurt parfaits, morning cereal, homemade muffins, or cookies.",
+
+"Oatmeal. Omega-3 fatty acids; magnesium; potassium; folate; niacin; calcium; soluble fiber. Top hot oatmeal with fresh berries. Oatmeal-and-raisin cookies are a hearty treat.",
+
+"Black or Kidney Beans. B-complex vitamins; niacin; folate; magnesium; omega-3 fatty acids; calcium; soluble fiber. Give soup or salad a nutrient boost -- stir in some beans",
+
+"Almonds. Plant omega-3 fatty acids; vitamin E; magnesium; fiber; heart-favorable mono- and polyunsaturated fats; phytosterols. Mix a few almonds (and berries) into low-fat yogurt, trail mix, or fruit salads.",
+
+"Walnuts. Plant omega-3 fatty acids; vitamin E; magnesium; folate; fiber; heart-favorable mono- and polyunsaturated fats; phytosterols. Walnuts add flavorful crunch to salads, pastas, cookies, muffins, even pancakes.", 
+
+"Red Wine. Catechins and reservatrol (flavonoids) Toast your good health! A glass of red wine could improve good HDL cholesterol.",
+
+"Tuna. Omega-3 fatty acids; folate; niacin. Here's lunch: Salad greens, fresh fruit, canned tuna. Keep Salad Spritzer - a light dressing -- in your office fridge.",
+
+"Tofu Niacin; folate; calcium; magnesium; potassium. Tasty tofu is easy: Thinly slice "firm" tofu, marinate several hours, grill or stir-fry.",
+
+"Brown rice. B-complex vitamins; fiber; niacin; magnesium, fiber. Microwavable brown rice makes a quick lunch. Stir in a few chopped veggies (broccoli, carrots, spinach).",
+
+"Soy milk. Isoflavones (a flavonoid); B-complex vitamins; niacin; folate, calcium; magnesium; potassium; phytoestrogens. Soy milk is great over oatmeal or whole-grain cereal. Or, make a smoothie with soy milk.",
+
+"Blueberries. Beta-carotene and lutein (carotenoids); anthocyanin (a flavonoid); ellagic acid (a polyphenol); vitamin C; folate; calcium, magnesium; potassium; fiber. Cranberries, strawberries, raspberries are potent, too -- for trail mixes, muffins, salads!.",
+
+"Carrots. Alpha-carotene (a carotenoid); fiber. Baby carrots are sweet for lunch. Sneak shredded carrots into spaghetti sauce or muffin batter.",
+
+"Spinach. Lutein (a carotenoid); B-complex vitamins; folate; magnesium; potassium; calcium; fiber. Pick spinach (not lettuce) for nutrient-packed salads and sandwiches.",
+
+"Broccoli. Beta-carotene (a carotenoid); Vitamins C and E; potassium; folate; calcium; fiber. Chop fresh broccoli into store-bought soup. For a veggie dip, try hummus (chickpeas).",
+
+"Sweet potato. Beta-carotene (a carotenoid); vitamins A, C, E; fiber. Microwave in a zip-lock baggie for lunch. Eat au naturale, or with pineapple bits.",
+
+"Red bell peppers. Beta-carotene and lutein (carotenoids); B-complex vitamins; folate; potassium; fiber. Rub with olive oil, and grill or oven-roast until tender. Delicious in wraps, salads, sandwiches.",
+
+"Asparagus. Beta-carotene and lutein (carotenoids); B-complex vitamins; folate; fiber. Grill or steam slightly, then dress with olive oil and lemon. It's a pretty side dish.",
+
+"Oranges Beta-cryptoxanthin. Beta- and alpha-carotene, lutein (carotenoids) and flavones (flavonoids); vitamin C; potassium; folate; fiber. Got orange juice? Check out the new nutrient-packed blends.",
+
+"Tomatoes. Beta- and alpha-carotene, lycopene, lutein (carotenoids); vitamin C; potassium; folate; fiber. For a flavor twist, try oil-packed tomatoes in sandwiches, salads, pastas, pizzas.",
+
+"Acorn squash. Beta-carotene and lutein (carotenoids); B-complex and C vitamins; folate; calcium; magnesium; potassium; fiber. Baked squash is comfort food on a chilly day. Serve with sautÃ©ed spinach, pine nuts, raisins."];
+	var arrayLen = recommendations.length; 
+	var randomNumber = Math.floor((Math.random()* arrayLen)+0);
+	return recommendations[randomNumber];	
 }
