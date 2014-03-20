3a8cac1b7d871c6328a83d6f3eb29228cfa3fb90
Dhuha <ranz25@gmail.com>
2014-03-20 17:11:35 -0300

    Jasmine test for login 1

---
+++ loginSpec.js
@@ -0,0 +1,47 @@
+
+describe("form", function() {
+
+    //vars for form component
+    var submitButton;
+    beforeEach(function() {
+        //get the correct compenents of the field
+        submitButton = document.getElementById('submit');
+        $(user_name).val("Channing");
+        $(password).val("hello");
+    })
+
+    it('should have non empty data',function(){
+        var username = $(user_name).val();             
+        var pass = $(password).val();
+        expect(username != null).toEqual(true);
+        expect(password != null).toEqual(true);
+    });
+	
+	
+	it('should create a cookie with non null username',function(){
+        submitButton.click();
+        var  cookieText = $.cookie("username");
+        console.log(cookieText == null);
+        expect(cookieText != null ).toEqual(true);
+    });
+})          
+
+
+
+describe("Server", function() {
+
+    //vars for form component
+    var username = "USER" + Math.random();
+    var tags = "lowIntensity";
+    beforeEach(function() {
+        //get the correct compenents of the field
+        submitButton = document.getElementById('submit');
+        
+    })
+    it('should contact the server',function(){
+
+        $.get("http://centi.cs.dal.ca:60000/hello",function(data,textStatus,jqXHR){
+            expect(data).toEqual('hello');
+        })
+    });
+})          
