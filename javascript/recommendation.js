685dcba686c551ab0c6ad796676b5b6bc9a3b42d
denays <denayscobas@live.ca>
2014-03-26 13:08:44 -0300

    Changes in 2 .js files and userpage.html
    recommendation.js had a change and delete it.
    survey.js  Parses the self rating nutritional data from the form and
    updates tags rating and uses the tag scores from the nutritional
    recommendation form data to generate a tag string, but it is missing the
    code for parsesing the nutritional recommendation data from the form and
    updates tags .
    userpage.html hada some css changes.

--- javascript/recommendation.js
+++ javascript/recommendation.js
@@ -15,8 +15,8 @@ $(document).ready( function() {
 	var user = $.cookie('username');
 	console.log("user: "  + user);
 
-	/**
-	* sends a get request to the server for the useres tags
+ /**
+  * sends a get request to the server for the useres tags
   * @method  get 
   * @param {String} url server reqest URL
   * @param {function} callback once the tags have been fetched
@@ -30,41 +30,42 @@ $(document).ready( function() {
 	//hide reccomendations that havent been tagged
 		console.log(lowCardio());
 		if(tags.indexOf("lowCardio") != -1){		
-			var curr = $("#recBox").html();
+			var curr = $("#whiteBox").html();
 			console.log("showing lowCardio: " + curr )
-			$("#recBox").html( curr + (lowCardio() + "<br/><br/>"));
+			$("#whiteBox").html( curr + (lowCardio() + "<br/><br/>"));
 		}
 
 		if(tags.indexOf("lowStrength") != -1){
 			console.log("showing lowStrength")
-			var curr = $("#recBox").html();
-			$("#recBox").html( curr + (lowStrength() + "<br/><br/>"));
+			var curr = $("#whiteBox").html();
+			$("#whiteBox").html( curr + (lowStrength() + "<br/><br/>"));
 		}
 
 		if(tags.indexOf("highCardio") != -1){
 			console.log("showing highCardio");
-			var curr = $("#recBox").html();
-			$("#recBox").html( curr + (highCardio() + "<br/><br/>"));
+			var curr = $("#whiteBox").html();
+			$("#whiteBox").html( curr + (highCardio() + "<br/><br/>"));
 		}
 
 		if(tags.indexOf("highStrength") != -1){
 			console.log("showing highStrength");
-			var curr = $("#recBox").html();
-			$("#recBox").html( curr + (highStrength() + "<br/><br/>"));
+			var curr = $("#whiteBox").html();
+			$("#whiteBox").html( curr + (highStrength() + "<br/><br/>"));
 		}
 
 		if(tags.indexOf("lowIntensity") != -1){
 			console.log("showing lowIntensity")
-			var curr = $("#recBox").html();
-			$("#recBox").html( curr + (lowIntensity() + "<br/><br/>"));
+			var curr = $("#whiteBox").html();
+			$("#whiteBox").html( curr + (lowIntensity() + "<br/><br/>"));
 
 		}
 
 		if(tags.indexOf("highIntensity") != -1){
 			console.log("showing highIntensity");
-			var curr = $("#recBox").html();
-			$("#recBox").html( curr + (highIntensity() + "<br/><br/>"));
+			var curr = $("#whiteBox").html();
+			$("#whiteBox").html( curr + (highIntensity() + "<br/><br/>"));
 		}
+		
 
 });
 })
